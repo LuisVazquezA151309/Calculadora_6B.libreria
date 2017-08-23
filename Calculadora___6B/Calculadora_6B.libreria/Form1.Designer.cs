49626079255f050183ed6869f5f30d2c353e437b
LuisVazquezA151309 <a151309@outlook.com>
2017-08-22 23:19:20 -0500

    Add files via upload

---
+++ Calculadora___6B/Calculadora_6B.libreria/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Calculadora_6B.libreria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txtnum1 = new System.Windows.Forms.TextBox();
+            this.Txtnum2 = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduca Valor_1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Introduca Valor_2";
+            // 
+            // Txtnum1
+            // 
+            this.Txtnum1.Location = new System.Drawing.Point(153, 81);
+            this.Txtnum1.Name = "Txtnum1";
+            this.Txtnum1.Size = new System.Drawing.Size(100, 22);
+            this.Txtnum1.TabIndex = 2;
+            // 
+            // Txtnum2
+            // 
+            this.Txtnum2.Location = new System.Drawing.Point(368, 81);
+            this.Txtnum2.Name = "Txtnum2";
+            this.Txtnum2.Size = new System.Drawing.Size(100, 22);
+            this.Txtnum2.TabIndex = 3;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(55, 202);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 38);
+            this.btnSumar.TabIndex = 4;
+            this.btnSumar.Text = "Suma";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(195, 202);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(75, 38);
+            this.btnRestar.TabIndex = 5;
+            this.btnRestar.Text = "Resta";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Location = new System.Drawing.Point(493, 202);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(111, 38);
+            this.btnMultiplicacion.TabIndex = 6;
+            this.btnMultiplicacion.Text = "Multiplicacíon";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(355, 202);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(75, 38);
+            this.btnDivision.TabIndex = 7;
+            this.btnDivision.Text = "Divisíon";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 298);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.Txtnum2);
+            this.Controls.Add(this.Txtnum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txtnum1;
+        private System.Windows.Forms.TextBox Txtnum2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnDivision;
+    }
+}
---
+++ Calculadora___6B/Calculadora_6B.libreria/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculadora_6B.libreria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1, num2, suma;
+            num1 = Convert.ToInt32(Txtnum1.Text);
+            num2 = Convert.ToInt32(Txtnum2.Text);
+            suma = num1 + num2;
+            MessageBox.Show("La suma es:"+suma,"Resultado:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resta;
+            num1 = Convert.ToInt32(Txtnum1.Text);
+            num2 = Convert.ToInt32(Txtnum2.Text);
+            resta = num1 - num2;
+            MessageBox.Show("La resta es:" + resta, "Resultado:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            int num1, num2, division;
+            num1 = Convert.ToInt32(Txtnum1.Text);
+            num2 = Convert.ToInt32(Txtnum2.Text);
+            division = num1 + num2;
+            MessageBox.Show("La division es:" +division, "Resultado:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            int num1, num2, multiplicacion;
+            num1 = Convert.ToInt32(Txtnum1.Text);
+            num2 = Convert.ToInt32(Txtnum2.Text);
+            multiplicacion = num1 * num2;
+            MessageBox.Show("La multiplicacion es:" + multiplicacion, "Resultado:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
